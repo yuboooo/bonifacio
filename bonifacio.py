25b0c41561475555f40d250a806259d5b3fcb9eb
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2022-11-12 23:24:22 -0500

    first commit

---
+++ bonifacio.py
@@ -0,0 +1,20 @@
+# imports
+
+
+# Check substitutable
+def check_substitable():
+    pass
+
+# Check LAD
+def check_lad():
+    pass
+
+# Many-many DA
+def defer_acceptance():
+    pass
+
+# Reduction
+def reduction():
+    # Run DA on firm side
+    # Run DA on worker side
+    pass
