e6ffe80e638358b8068476dee179b397fcbc67f0
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2022-11-15 11:29:10 -0500

    Modified da, make female_favorback function

--- da.py
+++ da.py
@@ -1,23 +1,37 @@
 from itertools import permutations
 
+# find the free male
 def male_without_match(male_matches, males):
     for male in males:
         if male not in male_matches:
             return male
     return None
 
+# for a specific male, find the female that the male has not proposed yet
 def female_not_proposed(male, male_proposed, male_pref):
     for female in male_pref: # for all female in male_pref list
         if male_proposed is [] or female not in male_proposed[male]: # if female not proposed by male before
             return female # return that female
     return "None" # if all female are proposed by the male before, it means no match
 
+# find a female that no male is proposed to yet
 def female_free(female_list, engaged):
     for female in female_list:
         if female in engaged:
             return False
     return True
 
+def female_favorback(male, female_list, females_prefs, female_engaged):
+    love = True # initialize love to true
+    for female in female_list: # for each female in male's highest rank female list
+        if female not in female_engaged:
+            female_engaged.extend([female])
+        female_pref = females_prefs[female] # get that female's preference list on male
+        if [male] not in female_pref: # if any female doesn't prefer the proposing male
+            love = False # set love to false
+    return love
+
+# if female is engaged with m', check whether she prefer m over m' or not
 def female_prefered(male, female, female_pref, female_matches):
     prefered = False
     prefered_matches = []
@@ -76,27 +90,18 @@ def defer_acceptance(males_prefs, females_prefs):
                     male_proposed[male].append(highest_rank_female)
 
             if female_free(highest_rank_female, female_engaged): # if that female is free
-                love = True # initialize love to true
-                for female in highest_rank_female: # for each female in male's highest rank female list
-                    female_pref = females_prefs[female] # get that female's preference list on male
-                    if [male] not in female_pref: # if any female doesn't prefer the proposing male
-                        love = False # set love to false
-                if love == True: # if love
+                love = female_favorback(male, highest_rank_female, females_prefs, female_engaged) # check whether female also favor the proposing male
+                if love == True: # if yes, then they engaged
                     male_matches[male] = highest_rank_female # engaged
-                    female_engaged.extend(highest_rank_female)
                     for female in highest_rank_female:
                         female_matches[female] = [male]
                 else: # male remains free, and propose to next prefered female
                     propose()
 
             else: # if the female is already engaged with other m'
-                love = True 
+                love = female_favorback(male, highest_rank_female, females_prefs, female_engaged) # check whether female also favor the proposing male
                 for female in highest_rank_female:
-                    if female not in female_engaged:
-                        female_engaged.extend([female])
                     female_pref = females_prefs[female]
-                    if [male] not in female_pref: # if any female doesn't prefer the proposing male
-                        love = False # set love to false
                     if love == True:
                         female_prefered_list[female] = female_prefered(male, female, female_pref, female_matches)
 
@@ -109,7 +114,6 @@ def defer_acceptance(males_prefs, females_prefs):
                     if prefered == True: # m > m'
                         for female, values in female_prefered_list.items(): # values = (prefered, prefered_matches, current_matches)
                             male_matches[male] = highest_rank_female
-
                             if values[0] == True:
                                 female_matches[female] = values[1] # female match with m
                     else:
@@ -119,5 +123,4 @@ def defer_acceptance(males_prefs, females_prefs):
         male_matches = dict(sorted(male_matches.items()))
         female_matches = dict(sorted(female_matches.items()))
 
-        
     return [male_matches, female_matches]
