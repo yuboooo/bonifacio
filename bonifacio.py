6178b76f18ce3fd24619e91f0ee2142a1132304e
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2022-12-01 02:17:04 -0500

    Add cycle structure

--- bonifacio.py
+++ bonifacio.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from defer_acceptance import defer_acceptance
 from reduction import reduction
 from input_check import substitutability, lad
+from cycle import find_cycle, run_cycle
 
 
 def bonifacio(fp, wp):
@@ -13,8 +14,7 @@ def bonifacio(fp, wp):
     check_lad = lad(fp) and lad(wp)
     if check_sub and check_lad:
         # Get firm's and worker's optimal stable matching
-        uf = defer_acceptance(fp, wp)
-        uw = defer_acceptance(wp, fp)
+        uf, uw = defer_acceptance(fp, wp)[0], defer_acceptance(wp, fp)[1]
         u_sets.append(uf)
         u_sets.append(uw)
         def cycle(uf, uw):
@@ -24,7 +24,7 @@ def bonifacio(fp, wp):
                 # Find cycle for new preference list
                 c_sets = find_cycle(new_fp, new_wp) # c_sets --> set of cycle consist of c1, c2 ... cn
                 # Get new set of stable matching from cycle we found (Substitute on uf)
-                new_u_sets = run_cycle(cycles)
+                new_u_sets = run_cycle(c_sets)
                 u_sets.extend(new_u_sets)
                 for new_u in new_u_sets:
                     cycle(new_u, uw)
---
+++ cycle.py
@@ -0,0 +1,5 @@
+def find_cycle(new_fp, new_wp):
+    pass
+
+def run_cycle(c_sets):
+    pass
