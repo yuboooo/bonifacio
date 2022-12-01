5fd6c1ec6f8a0009b50602b8190c96c6385e86fa
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2022-11-30 23:29:17 -0500

    Add lad test

--- test_bonifacio.py
+++ test_bonifacio.py
@@ -1,9 +1,10 @@
 from test_defer_acceptance import test_defer_acceptance
-from test_input_check import test_substitutability
+from test_input_check import test_substitutability, test_lad
 from test_reduction import test_reduction
 
 def test_bonifacio():
         test_substitutability()
+        test_lad()
         test_defer_acceptance()
         test_reduction()
         
